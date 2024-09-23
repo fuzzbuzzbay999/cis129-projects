83b1138f490f8be6c8df2cc9d309550fdbe6df90
fuzzbuzzbay999 <51093507+fuzzbuzzbay999@users.noreply.github.com>
2024-09-23 13:59:05 -0700

    Update cis129-lab02_ShortStory.py
    edited the code because I had to

--- cis129-lab02_ShortStory.py
+++ cis129-lab02_ShortStory.py
@@ -1,2 +1,16 @@
-print("Hello, World")
-print("if you are creating a file" + \n +"you dont have to commit after every line")
+# Author Declan Julinao
+# Not actually my first python script.
+# For the sake of this course it is
+# It ShoULD print a simple short story
+
+#since this isnt java, i guess i have to print chars instead of sting. 
+import time
+i=0
+print('Hello, World')
+print('if you are creating a file" + \n +"you dont have to commit after every line')
+while True:
+  print(i)
+  i+=1
+  if i>3000:
+    break
+print('much faster then java tho')
